80849fbba6706011aea78603bd4378256bb68398
klacsu <161206076+klacsu@users.noreply.github.com>
2024-07-17 19:21:30 +0200

    Update test_hootel.py

--- test/test_hootel.py
+++ test/test_hootel.py
@@ -14,8 +14,8 @@ class TestHootel(object):
         options.add_argument('--headless')
         self.browser = webdriver.Chrome(options=options)
         self.browser.get(URL)
-        self.browser.maximize_window()
-
+        # self.browser.maximize_window()
+        self.browser.set_window_size(1024, 800)
     def teardown_method(self):
         self.browser.quit()
 
@@ -24,8 +24,8 @@ class TestHootel(object):
     @allure.severity(allure.severity_level.TRIVIAL)
     @allure.tag("login")
     def test_login(self):
-        buci = self.browser.find_element(By.XPATH, "//link[@type='image/x-icon']")
-        buci.click()
+        # buci = self.browser.find_element(By.XPATH, "//link[@type='image/x-icon']")
+        # buci.click()
         login_btn = self.browser.find_element(By.XPATH, '//a[@class="nav-link"]')
         login_btn.click()
 
